170e73918b7c8fbc337487c4c87997e4915ccade
cornrowsMatt <mtuson1414@gmail.com>
2023-07-01 09:34:25 +1200

    routes for new components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {RandomComponent} from './random/random.component';
+import {BuildComponent} from './build/build.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'home'},
-  // { path: 'home', component: },
+  {path: 'home', component: HomeComponent},
+  {path: 'play', component: RandomComponent},
+  {path: 'build', component: BuildComponent},
 
 ];
 
