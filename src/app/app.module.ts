ca85f193e4b9e110b02ef5bf698516a75dc37cef
cornrowsMatt <mtuson1414@gmail.com>
2023-07-01 09:33:02 +1200

    generated components that will eventually be needed for the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+//  Components
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {RandomComponent} from './random/random.component';
+import {BuildComponent} from './build/build.component';
+
+//  Material Modules
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
@@ -15,7 +21,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, RandomComponent, BuildComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ src/app/build/build.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-build',
+  templateUrl: './build.component.html',
+  styleUrls: ['./build.component.scss']
+})
+export class BuildComponent {
+
+}
