a95250bc761b7c66107325def137013628c0db4f
cornrowsMatt <mtuson1414@gmail.com>
2023-07-01 12:26:08 +1200

    formatted app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,38 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppRoutingModule} from './app-routing.module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {HttpClientModule} from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 //  Components
-import {AppComponent} from './app.component';
-import {HomeComponent} from './home/home.component';
-import {BoardComponent} from './board/board.component';
-import {RandomComponent} from './random/random.component';
-import {BuildComponent} from './build/build.component';
-import {SquareComponent} from './square/square.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BoardComponent } from './board/board.component';
+import { RandomComponent } from './random/random.component';
+import { BuildComponent } from './build/build.component';
+import { SquareComponent } from './square/square.component';
 
 //  Material Modules
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatCardModule} from '@angular/material/card';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatMenuModule} from '@angular/material/menu'
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatMenuModule } from '@angular/material/menu';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, RandomComponent, BuildComponent, BoardComponent, SquareComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    RandomComponent,
+    BuildComponent,
+    BoardComponent,
+    SquareComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -40,6 +48,7 @@ import {MatInputModule} from '@angular/material/input';
     MatCheckboxModule,
     MatFormFieldModule,
     MatInputModule,
+    MatProgressBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
