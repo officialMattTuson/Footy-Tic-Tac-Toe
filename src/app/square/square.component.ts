c64c8045518a00cfe8e3788d8f84d5082571b135
cornrowsMatt <mtuson1414@gmail.com>
2023-07-01 12:24:11 +1200

    config fix

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./square.component.scss']
 })
 export class SquareComponent {
-
+  @Input() value!: 'X' | 'O';
 }
